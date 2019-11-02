700f315989a091188a10044f912603c2daf04def
siddacious <nospam187+github@gmail.com>
2019-11-02 09:50:38 -0700

    finished tester prototype

--- examples/lsm6dsox_tester.py
+++ examples/lsm6dsox_tester.py
@@ -7,31 +7,55 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 sox = adafruit_lsm6dsox.LSM6DSOX(i2c)
 
-#CS_5V  - LOW will disable I2C, puts in SPI mode
-#INT1 #float/internal pulldown= i2c& i3c :: HIGH = I3C only/I2C disabled
-    
-#SDO _sdo_pu_en
-sox._sdo_pu_en = True # toggle SD0
+print("Starting test")
+# most of this info is from the "Internal pin status" table
+# on page 42 of https://www.st.com/resource/en/datasheet/lsm6dsox.pdf
+
+# SETUP:
+# VIN - 5V
+# GND - GND
+# 3V0 - A0
+# SDA - A4
+# SCL - A5
+# SDO/DO, CS, INT1 INT2, SDO_AUX, OCS_AUX, SCX. SDX to digital IO
+
+#VIN/VCC - You'll know pretty quick if it's not working 
+# 3Vo - test with A0
+# GND see VIN
+# SDA \
+# SCL _\ Pull ups tested via ADC, function by usage
+
+#SDO Default: input without pull-up.
+sox._sdo_pu_en = True # enable pull-up
 time.sleep(0.010)
-#SCX &  #SDX _
-#access master registers
+
+#CS_5V  - LOW will disable I2C, puts in SPI mode
+
+#INT1  - HIGH Will disable I2C , puts in I3C 
+
+#INT2 - Default: output forced to ground
+sox._h_lactive = True # switch to hi-z 
+time.sleep(0.010)
+
+# SDO_AUX Default: input with pull-up.
+# OCS_AUX Default: input with pull-up.
+
+sox._ois_pu_dis = True # disable pull-up
+time.sleep(0.010)
+
+# SCX \
+# SDX _\ Turn on i2c master pullups 
+# enable i2c master register access
+# needed because the next register uses a duplicate address
 sox._shub_reg_access = True
 time.sleep(0.010)
-# enable pull up
+# enable pull ups
 sox._shub_pu_en = True
 time.sleep(0.010)
-#block master registers
+# disable i2c master register access
 sox._shub_reg_access = False
 time.sleep(0.010)
 
-#INT2
-sox._h_lactive = True
-time.sleep(0.010)
-#OCS_AUX& #SDO_AUX
-#sox._sim_ois = True
-#time.sleep(0.010)
+time.sleep(0.5) # hang around for a bit
 
-sox._ois_pu_dis = False
-time.sleep(0.010)
-
-time.sleep(0.2) # switch polarity of int2
+print("test done")

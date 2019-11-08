a4621a4eee3c8f27f9089b5f1a2e205a7f05e8dc
siddacious <nospam187+github@gmail.com>
2019-11-08 11:58:06 -0800

    rearranged tests for clarity and functional grouping

--- examples/lsm6dsox_full_test.py
+++ examples/lsm6dsox_full_test.py
@@ -9,16 +9,6 @@ sox = LSM6DSOX(i2c)
 sox.accelerometer_range = AccelRange.RANGE_16G
 print("Accelerometer range set to: %d G"%AccelRange.string[sox.accelerometer_range])
 
-sox.gyro_range = GyroRange.RANGE_500_DPS
-print("Gyro range set to: %d DPS"%GyroRange.string[sox.gyro_range])
-
-sox.accelerometer_rate = Rate.RATE_12_5_HZ
-print("Accelerometer rate set to: %d HZ"%Rate.string[sox.accelerometer_rate])
-
-sox.gyro_rate = Rate.RATE_12_5_HZ
-print("Gyro rate set to: %d HZ"%Rate.string[sox.gyro_rate])
-
-print()
 for accel_range in [AccelRange.RANGE_2G, AccelRange.RANGE_4G,
     AccelRange.RANGE_8G, AccelRange.RANGE_16G]:
 
@@ -27,13 +17,22 @@ for accel_range in [AccelRange.RANGE_2G, AccelRange.RANGE_4G,
         (sox.acceleration[2], AccelRange.string[sox.accelerometer_range]))
 
 print()
+sox.gyro_range = GyroRange.RANGE_500_DPS
+print("Gyro range set to: %d DPS"%GyroRange.string[sox.gyro_range])
 for gyro_range in [GyroRange.RANGE_250_DPS, GyroRange.RANGE_500_DPS,
     GyroRange.RANGE_1000_DPS, GyroRange.RANGE_2000_DPS]:
 
     sox.gyro_range = gyro_range
     print("Non movement is %.2f degrees/s when gyro range is %d DPS"%
         (sox.gyro[2], GyroRange.string[sox.gyro_range]))
+print()
+sox.accelerometer_rate = Rate.RATE_12_5_HZ
+print("Accelerometer rate set to: %d HZ"%Rate.string[sox.accelerometer_rate])
 
+sox.gyro_rate = Rate.RATE_12_5_HZ
+print("Gyro rate set to: %d HZ"%Rate.string[sox.gyro_rate])
+
+print()
 while True:
     print("Accel X:%.2f Y:%.2f Z:%.2f ms^2 Gyro X:%.2f Y:%.2f Z:%.2f degrees/s"%
         (sox.acceleration+sox.gyro))

df09e2746ef3b5b692870a75f14fe82b23e34cb1
siddacious <nospam187+github@gmail.com>
2019-11-08 11:36:06 -0800

    added range checks

--- examples/lsm6dsox_full_test.py
+++ examples/lsm6dsox_full_test.py
@@ -17,3 +17,19 @@ print("Accelerometer rate set to: %d HZ"%Rate.string[sox.accelerometer_rate])
 
 sox.gyro_rate = Rate.RATE_12_5_HZ
 print("Gyro rate set to: %d HZ"%Rate.string[sox.gyro_rate])
+
+print()
+for accel_range in [AccelRange.RANGE_2G, AccelRange.RANGE_4G,
+    AccelRange.RANGE_8G, AccelRange.RANGE_16G]:
+
+    sox.accelerometer_range = accel_range
+    print("Gravity is %.2f when accelerometer range is %d G"%
+        (sox.acceleration[2], AccelRange.string[sox.accelerometer_range]))
+
+print()
+for gyro_range in [GyroRange.RANGE_250_DPS, GyroRange.RANGE_500_DPS,
+    GyroRange.RANGE_1000_DPS, GyroRange.RANGE_2000_DPS]:
+
+    sox.gyro_range = gyro_range
+    print("Non movement is %.2f when gyro range is %d DPS"%
+        (sox.gyro[2], GyroRange.string[sox.gyro_range]))

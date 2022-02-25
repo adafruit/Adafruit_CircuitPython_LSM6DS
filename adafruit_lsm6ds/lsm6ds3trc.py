1a5fb365b0b67e2a767777370a2c60e121b875ac
myllyja <43068636+myllyja@users.noreply.github.com>
2022-02-25 20:21:30 +0200

    Fixed LSM6DS3 to LSM6DS3TR-C
    This chip has slightly different registers.

--- adafruit_lsm6ds/lsm6ds3trc.py
+++ adafruit_lsm6ds/lsm6ds3trc.py
@@ -2,36 +2,37 @@
 #
 # SPDX-License-Identifier: MIT
 """
-This module provides the `adafruit_lsm6ds.lsm6ds3` subclass of LSM6DS sensors
+This module provides the `adafruit_lsm6ds.lsm6ds3trc` subclass of LSM6DS sensors
 ===============================================================================
 """
-from . import LSM6DS
+from . import LSM6DS, RWBit, const
 
+_LSM6DS_CTRL10_C = const(0x19)
 
-class LSM6DS3(LSM6DS):  # pylint: disable=too-many-instance-attributes
+class LSM6DS3TRC(LSM6DS):  # pylint: disable=too-many-instance-attributes
 
-    """Driver for the LSM6DS3 6-axis accelerometer and gyroscope.
+    """Driver for the LSM6DS3TR-C 6-axis accelerometer and gyroscope.
 
-    :param ~busio.I2C i2c_bus: The I2C bus the LSM6DS3 is connected to.
+    :param ~busio.I2C i2c_bus: The I2C bus the LSM6DS3TR-C is connected to.
     :param int address: The I2C device address. Defaults to :const:`0x6A`
 
 
     **Quickstart: Importing and using the device**
 
-        Here is an example of using the :class:`LSM6DS3` class.
+        Here is an example of using the :class:`LSM6DS3TRC` class.
         First you will need to import the libraries to use the sensor
 
         .. code-block:: python
 
             import board
-            from adafruit_lsm6ds.lsm6ds3 import LSM6DS3
+            from adafruit_lsm6ds.lsm6ds3trc import LSM6DS3TRC
 
         Once this is done you can define your `board.I2C` object and define your sensor object
 
         .. code-block:: python
 
             i2c = board.I2C()  # uses board.SCL and board.SDA
-            sensor = LSM6DS3(i2c)
+            sensor = LSM6DS3TRC(i2c)
 
         Now you have access to the :attr:`acceleration` and :attr:`gyro`: attributes
 
@@ -43,3 +44,7 @@ class LSM6DS3(LSM6DS):  # pylint: disable=too-many-instance-attributes
     """
 
     CHIP_ID = 0x6A
+
+    # This version of the IMU has a different register for enabling the pedometer
+    # https://www.st.com/resource/en/datasheet/lsm6ds3tr-c.pdf
+    _ped_enable = RWBit(_LSM6DS_CTRL10_C, 4)

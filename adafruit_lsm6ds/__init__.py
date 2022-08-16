b768f7077c17cbb1e4d817e5a5b36bea511c9bf1
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:13 -0400

    Update version string

--- adafruit_lsm6ds/__init__.py
+++ adafruit_lsm6ds/__init__.py
@@ -52,7 +52,7 @@ Implementation Notes
 
 """
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_LSM6DS.git"
 
 import struct

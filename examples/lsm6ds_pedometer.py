426157efb35a1e9881ae5e91e1982fde7e830484
Dan Halbert <halbert@adafruit.com>
2025-11-25 18:06:44 -0500

    Pedometer example: specify device carefuly

--- examples/lsm6ds_pedometer.py
+++ examples/lsm6ds_pedometer.py
@@ -12,11 +12,17 @@ import board
 from adafruit_lsm6ds import AccelRange, Rate
 
 # pylint:disable=no-member
-from adafruit_lsm6ds.lsm6ds33 import LSM6DS33
+# Import the correct class for the sensor that you have.
+# Change the import as needed. Not all are listed here.
+# Note that the LSM6DS3TRC uses a different I2C register to enable the pedometer,
+# so the device classes are not interchangeable.
+from adafruit_lsm6ds.lsm6ds3trc import LSM6DS3TRC as LSM
+# from adafruit_lsm6ds.lsm6ds33 import LSM6DS33 as LSM
+
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
-sensor = LSM6DS33(i2c)
+sensor = LSM(i2c)
 
 # enable accelerometer sensor @ 2G and 26 Hz
 sensor.accelerometer_range = AccelRange.RANGE_2G

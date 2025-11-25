b45011b14e94a301b86da78348c1ca1cb8307e59
Dan Halbert <halbert@adafruit.com>
2025-11-25 18:13:54 -0500

    Update examples/lsm6ds_pedometer.py

--- examples/lsm6ds_pedometer.py
+++ examples/lsm6ds_pedometer.py
@@ -17,8 +17,9 @@ from adafruit_lsm6ds import AccelRange, Rate
 # Note that the LSM6DS3TRC, which is used on the Adafruit Feather Sense,
 # uses a different I2C register to enable the pedometer,
 # so the device classes are not interchangeable.
-from adafruit_lsm6ds.lsm6ds3trc import LSM6DS3TRC as LSM
+
 # from adafruit_lsm6ds.lsm6ds33 import LSM6DS33 as LSM
+from adafruit_lsm6ds.lsm6ds3trc import LSM6DS3TRC as LSM
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
